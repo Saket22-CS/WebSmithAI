7c0be310127ef5e8d297e691cac5515dd39c2a61
Saket Chaudhary <saketrishu64821@gmail.com>
2025-10-24 19:27:12 +0530

    some updates

--- context/OnSaveContext.tsx
+++ context/OnSaveContext.tsx
@@ -1,9 +1,9 @@
-"use client";
+// context/OnSaveContext.tsx
 import { createContext } from "react";
 
-export type OnSaveContextType = {
+export interface OnSaveContextType {
   onSaveData: any;
   setOnSaveData: React.Dispatch<React.SetStateAction<any>>;
-};
+}
 
 export const OnSaveContext = createContext<OnSaveContextType | null>(null);
